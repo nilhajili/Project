c81ba67e39d78646b7debb73ccc21cdd263ab3c1
nilhajili <hacilinilufer@gmail.com>
2026-05-18 21:45:52 +0400

    fix ci cd docker run latest tags4

--- ui/client/vite.config.js
+++ ui/client/vite.config.js
@@ -5,7 +5,12 @@ import babel from '@rolldown/plugin-babel'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    host: true,
+    port: 3000,
+  }
 })
