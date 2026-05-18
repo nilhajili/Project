1a0c35f54929d349bacbe4409957569672198434
nilhajili <hacilinilufer@gmail.com>
2026-05-18 23:05:42 +0400

    fixed backend sql problem

--- backend/Program.cs
+++ backend/Program.cs
@@ -126,6 +126,12 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate(); 
+}
+
 #region Middleware
 
 if (app.Environment.IsDevelopment())
